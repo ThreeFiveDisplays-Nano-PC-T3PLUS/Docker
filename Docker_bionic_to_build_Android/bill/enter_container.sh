2846171afa14cfacff0faaf6026f4d321c74d2fa
ThreeFiveDisplays-Nano-PC-T3PLUS <88690798+ThreeFiveDisplays-Nano-PC-T3PLUS@users.noreply.github.com>
2022-02-01 14:10:20 -0700

    Delete Docker_bionic_to_build_Android directory

--- Docker_bionic_to_build_Android/bill/enter_container.sh
+++
@@ -1,2 +0,0 @@
-docker run -it -e USER=buildfarm  --entrypoint /bin/bash  -v /Volumes/case-sensitive/android_build/NanoPC_t3_plus/:/home/tugy/develop/nanopi_t3plus_v7.1/  -t ubuntu-bionic:android-nanopi-t3plus
-ls -l
--- Docker_bionic_to_build_Android/mark/make_container.sh
+++
@@ -1,1 +0,0 @@
- docker build -t "ubuntu-bionic:android-nanopi-t3plus" .
