15c1941a2da28a5af1e29904a6d886cfa67db465
ThreeFiveDisplays-Nano-PC-T3PLUS <88690798+ThreeFiveDisplays-Nano-PC-T3PLUS@users.noreply.github.com>
2022-02-01 14:54:51 -0700

    Update enter_container.sh

--- Docker_to_build_kernel_and_uboot/mark/enter_container.sh
+++ Docker_to_build_kernel_and_uboot/mark/enter_container.sh
@@ -1,5 +1,5 @@
 cd Docker/Docker_to_build_kernel_and_uboot/mark
-docker run -it -e USER=root  --privileged=true --entrypoint=/bin/bash -v /media/mark/projects/T3plus/:/home/tugy/develop/nano_t3_plus/ -t ubuntu-bionic:NanoPC_T3
+docker run -it -e USER=root  --privileged=true --entrypoint=/bin/bash -v /media/mark/projects/T3plus/:/home/tugy/develop/nano_t3_plus/s5pxx18-android-7/ -t ubuntu-bionic:NanoPC_T3
 ls -l
 cd ..
 cd ..
